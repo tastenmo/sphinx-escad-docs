581dedf49a685f0dcaa4172a9fc77e2becbeb8a6
heubuchm <martin.heubuch@escad.de>
2023-10-12 10:44:57 +0000

    added pyproject

--- demo/conf.py
+++ demo/conf.py
@@ -87,7 +87,7 @@ simplepdf_vars = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'escaddocs_theme'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
 html_logo = './_static/REscad.svg'
