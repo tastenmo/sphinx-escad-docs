8adb503f48cd0374335c20be4b9e90706d42a752
Martin Heubuch <martin.heubuch@escad.de>
2023-11-13 14:29:38 +0100

    changed python version

--- demo/conf.py
+++ demo/conf.py
@@ -86,7 +86,7 @@ escaddocs_vars = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
 
 html_logo = './_static/REscad.svg'
